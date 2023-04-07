4827014ee6e60219bc34df5221a5578b48914d57
Krysh <krysh.developer@gmail.com>
2023-04-07 12:47:18 +0200

    [DEV-1462] disabled sell button

--- src/components/service-button.tsx
+++ src/components/service-button.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import DfxIcon, { IconVariant } from '../stories/DfxIcon';
+import DfxIcon, { IconColors, IconVariant } from '../stories/DfxIcon';
 
 export enum ServiceButtonType {
   BUY = 'Buy',
@@ -10,31 +10,32 @@ export enum ServiceButtonType {
 interface ServiceButtonProps {
   type: ServiceButtonType;
   url: string;
+  disabled?: boolean;
 }
 
-export function ServiceButton({ type, url }: ServiceButtonProps): JSX.Element {
+export function ServiceButton({ type, url, disabled }: ServiceButtonProps): JSX.Element {
   const navigate = useNavigate();
 
-  const iconDefinitions: Record<ServiceButtonType, JSX.Element> = {
-    [ServiceButtonType.BUY]: (
+  const iconDefinitions: Record<ServiceButtonType, (color: IconColors) => JSX.Element> = {
+    [ServiceButtonType.BUY]: (color: IconColors) => (
       <>
-        <DfxIcon icon={IconVariant.BANK} />
-        <DfxIcon icon={IconVariant.ARROW_RIGHT} />
-        <DfxIcon icon={IconVariant.WALLET} />
+        <DfxIcon icon={IconVariant.BANK} color={color} />
+        <DfxIcon icon={IconVariant.ARROW_RIGHT} color={color} />
+        <DfxIcon icon={IconVariant.WALLET} color={color} />
       </>
     ),
-    [ServiceButtonType.SELL]: (
+    [ServiceButtonType.SELL]: (color: IconColors) => (
       <>
-        <DfxIcon icon={IconVariant.WALLET} />
-        <DfxIcon icon={IconVariant.ARROW_RIGHT} />
-        <DfxIcon icon={IconVariant.BANK} />
+        <DfxIcon icon={IconVariant.WALLET} color={color} />
+        <DfxIcon icon={IconVariant.ARROW_RIGHT} color={color} />
+        <DfxIcon icon={IconVariant.BANK} color={color} />
       </>
     ),
-    [ServiceButtonType.CONVERT]: (
+    [ServiceButtonType.CONVERT]: (color: IconColors) => (
       <>
-        <DfxIcon icon={IconVariant.CIRCLE} />
-        <DfxIcon icon={IconVariant.SWAP} />
-        <DfxIcon icon={IconVariant.CIRCLE_OUTLINE} />
+        <DfxIcon icon={IconVariant.CIRCLE} color={color} />
+        <DfxIcon icon={IconVariant.SWAP} color={color} />
+        <DfxIcon icon={IconVariant.CIRCLE_OUTLINE} color={color} />
       </>
     ),
   };
@@ -44,9 +45,10 @@ export function ServiceButton({ type, url }: ServiceButtonProps): JSX.Element {
       className="flex flex-col gap-2 items-center justify-center rounded border border-dfxGray-300 h-20 w-60 shadow-dfx"
       type="button"
       onClick={() => navigate(url)}
+      disabled={disabled}
     >
-      <p className="text-dfxBlue-800 text-lg font-bold">{type}</p>
-      <div className="flex flex-row gap-2">{iconDefinitions[type]}</div>
+      <p className={`${disabled ? 'text-dfxGray-500' : 'text-dfxBlue-800'} text-lg font-bold`}>{type}</p>
+      <div className="flex flex-row gap-2">{iconDefinitions[type](disabled ? IconColors.GRAY : IconColors.RED)}</div>
     </button>
   );
 }
