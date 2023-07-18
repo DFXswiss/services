f3584f27777872ca5b8af99b17cbff4e09e66efe
David May <david.leo.may@gmail.com>
2023-07-18 14:50:01 +0200

    [NO-TASK] Reset session, if login fails

--- src/hooks/url-param-helper.hook.ts
+++ src/hooks/url-param-helper.hook.ts
@@ -2,21 +2,22 @@ import { Utils, useApiSession } from '@dfx.swiss/react';
 import { useAppHandlingContext } from '../contexts/app-handling.context';
 import { useBalanceContext } from '../contexts/balance.context';
 import { useQuery } from './query.hook';
+import { useMemo } from 'react';
 
 interface UrlParamHelperInterface {
   readParamsAndReload: () => Promise<void>;
 }
 
 export function useUrlParamHelper(): UrlParamHelperInterface {
-  const { updateSession, createSession } = useApiSession();
+  const { updateSession, deleteSession, createSession } = useApiSession();
   const { setRedirectUri } = useAppHandlingContext();
   const { readBalances } = useBalanceContext();
   const { address, signature, session, redirectUri, balances, reloadWithoutBlockedParams } = useQuery();
 
   async function readParamsAndReload() {
     if (address && signature) {
-      const session = await createSession(address, signature, false);
-      if (session) updateSession(session);
+      const session = await createSession(address, signature, false).catch(() => undefined);
+      session ? updateSession(session) : deleteSession();
     }
     if (session && Utils.isJwt(session)) {
       updateSession(session);
@@ -30,5 +31,5 @@ export function useUrlParamHelper(): UrlParamHelperInterface {
     reloadWithoutBlockedParams();
   }
 
-  return { readParamsAndReload };
+  return useMemo(() => ({ readParamsAndReload }), []);
 }
