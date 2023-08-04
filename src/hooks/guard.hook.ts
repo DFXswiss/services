9fd856e726818030186b597c3e2f164f9d50ee42
David May <david.leo.may@gmail.com>
2023-08-04 15:40:17 +0200

    [DEV-1680] Fixed KYC data guard, added autocomplete tags

--- src/hooks/guard.hook.ts
+++ src/hooks/guard.hook.ts
@@ -1,5 +1,6 @@
 import { useSessionContext, useUserContext } from '@dfx.swiss/react';
 import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { useParamContext } from '../contexts/param.context';
 import { useNavigation } from './navigation.hook';
 
@@ -16,8 +17,10 @@ export function useSessionGuard(redirectPath = '/') {
 export function useKycDataGuard(redirectPath = '/') {
   const { user, isUserLoading } = useUserContext();
   const { navigate } = useNavigation();
+  const { pathname } = useLocation();
 
   useEffect(() => {
-    if (user && !isUserLoading && !user.kycDataComplete) navigate(redirectPath);
+    if (user && !isUserLoading && !user.kycDataComplete)
+      navigate({ pathname: redirectPath, search: `?${new URLSearchParams({ 'redirect-path': pathname })}` });
   }, [user, isUserLoading, navigate]);
 }
