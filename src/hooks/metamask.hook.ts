3387a2664d3a279e4f290f60412347d4cddbf72b
David May <david.leo.may@gmail.com>
2023-08-08 15:45:20 +0200

    [DEV-1633] Fixed account switches

--- src/hooks/metamask.hook.ts
+++ src/hooks/metamask.hook.ts
@@ -170,9 +170,11 @@ export function useMetaMask(): MetaMaskInterface {
 
   async function readBalance(asset: Asset, address?: string): Promise<AssetBalance> {
     if (!address || !asset) return { asset, amount: 0 };
+
     if (asset.type === AssetType.COIN) {
       return web3.eth.getBalance(address).then((balance) => ({ asset, amount: toUsableNumber(balance).toNumber() }));
     }
+
     try {
       const tokenContract = createContract(asset.chainId);
       const decimals = await tokenContract.methods.decimals().call();
