bed43bb5d91006fdfccd3dec56ebb7633469a008
David May <david.leo.may@gmail.com>
2023-08-25 17:10:28 +0200

    [NO-TASK] Refactoring, updated PRD workflow

--- src/Main.widget.tsx
+++ src/Main.widget.tsx
@@ -5,6 +5,10 @@ function MainWidget(params: WidgetParams) {
   return (
     <>
       <link type="text/css" rel="stylesheet" href="main-widget.css" />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+      />
       <App routerFactory={createMemoryRouter} params={params} />
     </>
   );
