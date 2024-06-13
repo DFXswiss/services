c97ce4f3b25d30b2ebeef7afccc52dbb929e633c
lapatric <42653152+lapatric@users.noreply.github.com>
2024-06-13 10:01:12 +0200

    add error handling + fix small review issues

--- src/hooks/wallets/wallet-connect.hook.ts
+++ src/hooks/wallets/wallet-connect.hook.ts
@@ -1,5 +1,6 @@
 import { Asset, AssetType, Blockchain } from '@dfx.swiss/react';
 import {
+  BaseError,
   Connector,
   getAccount,
   getBalance,
@@ -19,6 +20,7 @@ import { useEffect, useMemo, useState } from 'react';
 import { isMobile } from 'react-device-detect';
 import { formatUnits, parseEther, parseUnits } from 'viem';
 import { AssetBalance } from '../../contexts/balance.context';
+import ERC20_ABI from '../../static/erc20.abi.json';
 import { config } from '../../wagmi.config';
 import { useWeb3 } from '../web3.hook';
 
@@ -109,87 +111,129 @@ export function useWalletConnect(): WalletConnectInterface {
   }
 
   async function connect(blockchain: Blockchain, onConnectUri: (uri: string) => void): Promise<string> {
-    await disconnect();
-    const connector = getWalletConnectConnector();
-    const provider = (await connector.getProvider()) as any;
-    provider.events.on('display_uri', onConnectUri);
+    try {
+      await disconnect();
+      const connector = getWalletConnectConnector();
+      const provider = (await connector.getProvider()) as any;
+      provider.events.on('display_uri', onConnectUri);
 
-    const { accounts } = await wagmiConnect(config, {
-      chainId: toChainId(blockchain) as any,
-      connector,
-    });
+      const { accounts } = await wagmiConnect(config, {
+        chainId: toChainId(blockchain) as any,
+        connector,
+      });
 
-    return accounts[0];
+      return accounts[0];
+    } catch (error) {
+      handleError(error);
+    }
   }
 
   async function disconnect(): Promise<void> {
-    await wagmiDiconnect(config, { connector: getWalletConnectConnector() }).catch((e) => console.error(e));
+    try {
+      await wagmiDiconnect(config, { connector: getWalletConnectConnector() });
+    } catch (error) {
+      handleError(error);
+    }
   }
 
   async function reconnect(): Promise<void> {
-    const { isDisconnected } = getAccount(config);
-    if (isDisconnected) {
-      await wagmiReconnect(config, { connectors: [getWalletConnectConnector()] });
+    try {
+      const { isDisconnected } = getAccount(config);
+      if (isDisconnected) {
+        await wagmiReconnect(config, { connectors: [getWalletConnectConnector()] });
+      }
+    } catch (error) {
+      handleError(error);
     }
   }
 
   async function signMessage(msg: string, address: string, _blockchain: Blockchain): Promise<string> {
-    return await wagmiSignMessage(config, {
-      message: msg,
-      account: address as any,
-      connector: getWalletConnectConnector(),
-    });
+    try {
+      return await wagmiSignMessage(config, {
+        message: msg,
+        account: address as any,
+        connector: getWalletConnectConnector(),
+      });
+    } catch (error) {
+      handleError(error);
+    }
   }
 
   async function readBalance(asset: Asset, address?: string): Promise<AssetBalance> {
     if (!address || !asset) return { asset, amount: 0 };
 
-    const balance = await getBalance(config, {
-      address: address as any,
-      token: asset.type === AssetType.COIN ? undefined : (asset.chainId as any),
-      chainId: Number(toChainId(asset.blockchain)) as any,
-    });
-
-    return { asset, amount: Number(formatUnits(balance.value, balance.decimals)) };
+    try {
+      const balance = await getBalance(config, {
+        address: address as any,
+        token: asset.type === AssetType.COIN ? undefined : (asset.chainId as any),
+        chainId: Number(toChainId(asset.blockchain)) as any,
+      });
+      return { asset, amount: Number(formatUnits(balance.value, balance.decimals)) };
+    } catch (error) {
+      return { asset, amount: 0 };
+    }
   }
 
   async function requestChangeToBlockchain(blockchain?: Blockchain): Promise<void> {
     if (!blockchain) return;
     const chainId = Number(toChainId(blockchain));
-    if (getAccount(config).chainId !== chainId) {
-      await switchChain(config, { chainId: chainId as any, connector: getWalletConnectConnector() });
-      await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    try {
+      if (getAccount(config).chainId !== chainId) {
+        await switchChain(config, { chainId: chainId as any, connector: getWalletConnectConnector() });
+        await new Promise((resolve) => setTimeout(resolve, 5000)); // delay to allow chain switch in wallet
+      }
+    } catch (error) {
+      handleError(error);
     }
   }
 
   async function createTransaction(amount: BigNumber, asset: Asset, from: string, to: string): Promise<string> {
-    if (asset.type === AssetType.COIN) {
-      return await sendTransaction(config, {
-        connector: getWalletConnectConnector(),
-        account: from as any,
-        chainId: Number(toChainId(asset.blockchain)) as any,
-        to: to as any,
-        value: parseEther(amount.toString()),
-        data: '0x', // needed for Trust Wallet
-      });
+    try {
+      if (asset.type === AssetType.COIN) {
+        return await sendTransaction(config, {
+          connector: getWalletConnectConnector(),
+          account: from as any,
+          chainId: Number(toChainId(asset.blockchain)) as any,
+          to: to as any,
+          value: parseEther(amount.toString()),
+          data: '0x', // needed for Trust Wallet
+        });
+      } else {
+        const decimals = (await readContract(config, {
+          abi: ERC20_ABI,
+          address: asset.chainId as any,
+          functionName: 'decimals',
+          chainId: Number(toChainId(asset.blockchain)) as any,
+        })) as number;
+
+        const { request } = await simulateContract(config, {
+          abi: ERC20_ABI,
+          chainId: Number(toChainId(asset.blockchain)) as any,
+          address: asset.chainId as any,
+          functionName: 'transfer',
+          args: [to as any, parseUnits(amount.toString(), decimals)],
+          connector: getWalletConnectConnector(),
+        });
+
+        return await writeContract(config, request);
+      }
+    } catch (error) {
+      handleError(error);
+    }
+  }
+
+  function handleError(error: any): never {
+    const baseError = error as BaseError;
+    let finalError: unknown = baseError;
+
+    baseError.walk((e) => e instanceof Error);
+    finalError = baseError;
+
+    if (finalError instanceof BaseError || finalError instanceof Error) {
+      throw new Error((finalError as BaseError).shortMessage);
     } else {
-      const decimals = await readContract(config, {
-        abi: erc20ABI,
-        address: asset.chainId as any,
-        functionName: 'decimals',
-        chainId: Number(toChainId(asset.blockchain)) as any,
-      });
-
-      const { request } = await simulateContract(config, {
-        abi: erc20ABI,
-        chainId: Number(toChainId(asset.blockchain)) as any,
-        address: asset.chainId as any,
-        functionName: 'transfer',
-        args: [to as any, parseUnits(amount.toString(), decimals)],
-        connector: getWalletConnectConnector(),
-      });
-
-      return await writeContract(config, request);
+      throw new Error('An unexpected error occurred.');
     }
   }
 
