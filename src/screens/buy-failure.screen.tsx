cd089ce95bf4eaa8c86de2735a9dd3b5d64e16d3
lapatric <42653152+lapatric@users.noreply.github.com>
2024-06-19 19:10:19 +0200

    simple buy/failure screen

---
+++ src/screens/buy-failure.screen.tsx
@@ -0,0 +1,34 @@
+import {
+  DfxIcon,
+  IconColor,
+  IconSize,
+  IconVariant,
+  StyledButton,
+  StyledButtonWidth,
+  StyledVerticalStack,
+} from '@dfx.swiss/react-components';
+import { Layout } from '../components/layout';
+import { useSettingsContext } from '../contexts/settings.context';
+import { useNavigation } from '../hooks/navigation.hook';
+
+export function BuyFailureScreen(): JSX.Element {
+  const { translate } = useSettingsContext();
+  const { navigate } = useNavigation();
+  const header = translate('screens/payment', 'Your payment has failed. Please try again.');
+
+  return (
+    <Layout title={translate('screens/buy', 'Credit Card Payment')} backButton={false} textStart>
+      <StyledVerticalStack gap={4}>
+        <div className="mx-auto">
+          <DfxIcon size={IconSize.XXL} icon={IconVariant.PROCESS_DONE} color={IconColor.BLUE} />
+        </div>
+        <p className="text-base font-bold text-center text-dfxBlue-800">{header}</p>
+        <StyledButton
+          label={translate('general/actions', 'Try again')}
+          onClick={() => navigate('/buy')}
+          width={StyledButtonWidth.FULL}
+        />
+      </StyledVerticalStack>
+    </Layout>
+  );
+}
