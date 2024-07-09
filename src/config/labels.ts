e56aa1cafcb174bde36622a99b5dbe9f502a08dc
David May <85513542+davidleomay@users.noreply.github.com>
2024-07-09 12:50:49 +0200

    [NO-TASK] Added transaction errors (#311)

--- src/config/labels.ts
+++ src/config/labels.ts
@@ -56,6 +56,8 @@ export const PaymentFailureReasons = {
   [TransactionFailureReason.INSTANT_PAYMENT]: 'Instant payment',
   [TransactionFailureReason.FEE_TOO_HIGH]: 'Network fee too high',
   [TransactionFailureReason.RECEIVER_REJECTED]: 'Payment rejected by receiver node',
+  [TransactionFailureReason.ASSET_KYC_NEEDED]: 'Asset requires KYC',
+  [TransactionFailureReason.CHF_ABROAD_NOT_ALLOWED]: 'CHF abroad not allowed',
 };
 
 // --- Limit increase requestS --- //
