6c9c0ed365a61b81e183dc095cb7e9df14133412
lapatric <42653152+lapatric@users.noreply.github.com>
2024-07-22 09:00:01 +0200

    adjust displayed data + error handling

--- src/screens/payment-routes.screen.tsx
+++ src/screens/payment-routes.screen.tsx
@@ -1,4 +1,4 @@
-import { Asset, Blockchain, Fiat, useApi, useUserContext } from '@dfx.swiss/react';
+import { ApiError, Asset, Blockchain, Fiat, useApi, useUserContext } from '@dfx.swiss/react';
 import {
   CopyButton,
   StyledDataTable,
@@ -10,6 +10,7 @@ import { useEffect, useRef, useState } from 'react';
 import { Layout } from 'src/components/layout';
 import { useSettingsContext } from 'src/contexts/settings.context';
 import { useBlockchain } from 'src/hooks/blockchain.hook';
+import { ErrorHint } from '../components/error-hint';
 
 export interface MinAmount {
   amount: number;
@@ -38,7 +39,7 @@ export interface BuyRoute {
 export interface SellRoute {
   id: number;
   active: boolean;
-  deposit: any;
+  deposit: DepositDto;
   iban: string;
   currency: Fiat;
   volume: number;
@@ -66,6 +67,8 @@ export interface RoutesDto {
   swap: SwapRoute[];
 }
 
+export type Route = BuyRoute | SellRoute | SwapRoute;
+
 export function PaymentRoutes(): JSX.Element {
   const { call } = useApi();
   const { translate } = useSettingsContext();
@@ -74,106 +77,107 @@ export function PaymentRoutes(): JSX.Element {
   const { user } = useUserContext();
   const { toString } = useBlockchain();
 
+  const [error, setError] = useState<string>();
+
   useEffect(() => {
     if (!user) return;
-    getBuyRoutes()
-      .then((routes: any) => {
-        console.log(routes);
+    getRoutes()
+      .then((routes: RoutesDto) => {
+        routes.buy = routes.buy.filter((route) => route.active);
+        routes.sell = routes.sell.filter((route) => route.active);
+        routes.swap = routes.swap.filter((route) => route.active);
         setRoutes(routes);
       })
-      .catch((error) => console.log(error));
+      .catch((error: ApiError) => setError(error.message ?? 'Unknown error'));
   }, [user]);
 
-  async function getBuyRoutes() {
-    console.log('Calling getBuyRoutes');
+  async function getRoutes(): Promise<RoutesDto> {
     return call({ url: '/route', method: 'GET' });
   }
 
   return (
     <Layout title={translate('navigation/links', 'Payment routes')} onBack={undefined} textStart rootRef={rootRef}>
-      <StyledVerticalStack full gap={4}>
-        {routes?.buy.length && (
-          <StyledDataTable label={translate('general/links', 'Buy')}>
-            {routes.buy.map<JSX.Element>((route) => (
-              <StyledDataTableExpandableRow
-                label={`${route.asset.blockchain} / ${route.asset.name}`}
-                expansionItems={[
-                  { label: translate('screens/payment', 'Asset'), text: route.asset.name },
-                  { label: translate('screens/payment', 'Blockchain'), text: route.asset.blockchain },
-                  { label: translate('screens/payment', 'Purpose of payment'), text: route.bankUsage },
-                  {
-                    label: translate('screens/payment', 'Minimum deposit'),
-                    text: `${route.minDeposits.map((d) => `${d.amount} ${d.asset}`).join(', ')}`,
-                  },
-                  {
-                    label: translate('screens/payment', 'Volume'),
-                    text: `${route.volume} CHF`,
-                  },
-                  {
-                    label: translate('screens/payment', 'Annual volume'),
-                    text: `${route.annualVolume} CHF`,
-                  },
-                ]}
-              >
-                {route.bankUsage}
-                <CopyButton onCopy={() => copy(route.bankUsage)} />
-              </StyledDataTableExpandableRow>
-            ))}
-          </StyledDataTable>
-        )}
-        {routes?.sell.length && (
-          <StyledDataTable label={translate('general/links', 'Sell')}>
-            {routes.sell.map<JSX.Element>((route) => (
-              <StyledDataTableExpandableRow
-                label={`${route.currency.name} / ${route.iban}`}
-                expansionItems={[
-                  { label: translate('screens/payment', 'Currency'), text: route.currency.name },
-                  { label: translate('screens/payment', 'IBAN'), text: route.iban },
-                  {
-                    label: translate('screens/payment', 'Deposit address'),
-                    text: `${route.minDeposits.map((d) => `${d.amount} ${d.asset}`).join(', ')}`,
-                  },
-                  {
-                    label: translate('screens/payment', 'Volume'),
-                    text: `${route.annualVolume} CHF`,
-                  },
-                ]}
-              >
-                <p className="text-right overflow-ellipsis">
-                  {route.deposit.blockchains.map((blockchain: Blockchain) => toString(blockchain)).join(', ')}
-                </p>
-              </StyledDataTableExpandableRow>
-            ))}
-          </StyledDataTable>
-        )}
-        {routes?.swap.length && (
-          <StyledDataTable label={translate('general/links', 'Swap')}>
-            {routes.swap.map<JSX.Element>((route) => (
-              <StyledDataTableExpandableRow
-                label={route.asset.name}
-                expansionItems={[
-                  { label: translate('screens/payment', 'Asset'), text: route.asset.name },
-                  { label: translate('screens/payment', 'Blockchain'), text: route.asset.blockchain },
-                  {
-                    label: translate('screens/payment', 'Minimum deposit'),
-                    text: `${route.minDeposits.map((d) => `${d.amount} ${d.asset}`).join(', ')}`,
-                  },
-                  {
-                    label: translate('screens/payment', 'Volume'),
-                    text: `${route.volume} CHF`,
-                  },
-                  {
-                    label: translate('screens/payment', 'Annual volume'),
-                    text: `${route.annualVolume} CHF`,
-                  },
-                ]}
-              >
-                {route.asset.name}
-              </StyledDataTableExpandableRow>
-            ))}
-          </StyledDataTable>
-        )}
-      </StyledVerticalStack>
+      {error ? (
+        <ErrorHint message={error} />
+      ) : (
+        <StyledVerticalStack full gap={4}>
+          {routes?.buy.length && (
+            <StyledDataTable label={translate('general/links', 'Buy')}>
+              {routes.buy.map<JSX.Element>((route) => (
+                <StyledDataTableExpandableRow
+                  label={`${route.asset.blockchain} / ${route.asset.name}`}
+                  expansionItems={[
+                    { label: translate('screens/payment', 'Asset'), text: route.asset.name },
+                    { label: translate('screens/payment', 'Blockchain'), text: route.asset.blockchain },
+                    { label: translate('screens/payment', 'Purpose of payment'), text: route.bankUsage },
+                    {
+                      label: translate('screens/payment', 'Volume'),
+                      text: `${route.volume} CHF`,
+                    },
+                    {
+                      label: translate('screens/payment', 'Annual volume'),
+                      text: `${route.annualVolume} CHF`,
+                    },
+                  ]}
+                >
+                  {route.bankUsage}
+                  <CopyButton onCopy={() => copy(route.bankUsage)} />
+                </StyledDataTableExpandableRow>
+              ))}
+            </StyledDataTable>
+          )}
+          {routes?.sell.length && (
+            <StyledDataTable label={translate('general/links', 'Sell')}>
+              {routes.sell.map<JSX.Element>((route) => (
+                <StyledDataTableExpandableRow
+                  label={`${route.currency.name} / ${route.iban}`}
+                  expansionItems={[
+                    { label: translate('screens/payment', 'Currency'), text: route.currency.name },
+                    { label: translate('screens/payment', 'IBAN'), text: route.iban },
+                    {
+                      label: translate('screens/payment', 'Annual volume'),
+                      text: `${route.annualVolume} CHF`,
+                    },
+                  ]}
+                >
+                  <p className="text-right overflow-ellipsis">
+                    {route.deposit.blockchains.map((blockchain: Blockchain) => toString(blockchain)).join(', ')}
+                  </p>
+                </StyledDataTableExpandableRow>
+              ))}
+            </StyledDataTable>
+          )}
+          {routes?.swap.length && (
+            <StyledDataTable label={translate('general/links', 'Swap')}>
+              {routes.swap.map<JSX.Element>((route) => (
+                <StyledDataTableExpandableRow
+                  label={route.asset.name}
+                  expansionItems={[
+                    { label: translate('screens/payment', 'Asset'), text: route.asset.name },
+                    { label: translate('screens/payment', 'Blockchain'), text: route.asset.blockchain },
+                    {
+                      label: translate('screens/payment', 'Deposit address'),
+                      text: route.deposit.address,
+                    },
+                    {
+                      label: translate('screens/payment', 'Volume'),
+                      text: `${route.volume} CHF`,
+                    },
+                    {
+                      label: translate('screens/payment', 'Annual volume'),
+                      text: `${route.annualVolume} CHF`,
+                    },
+                  ]}
+                >
+                  <p className="text-right overflow-ellipsis">
+                    {route.deposit.blockchains.map((blockchain: Blockchain) => toString(blockchain)).join(', ')}
+                  </p>
+                </StyledDataTableExpandableRow>
+              ))}
+            </StyledDataTable>
+          )}
+        </StyledVerticalStack>
+      )}
     </Layout>
   );
 }
