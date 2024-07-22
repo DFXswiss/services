1303360b3d2115a7cb2a265b12b5d48656b2ce5d
lapatric <42653152+lapatric@users.noreply.github.com>
2024-07-22 09:18:52 +0200

    add translations

--- src/screens/payment-routes.screen.tsx
+++ src/screens/payment-routes.screen.tsx
@@ -96,22 +96,22 @@ export function PaymentRoutes(): JSX.Element {
   }
 
   return (
-    <Layout title={translate('navigation/links', 'Payment routes')} onBack={undefined} textStart rootRef={rootRef}>
+    <Layout title={translate('screens/payment', 'Payment routes')} onBack={undefined} textStart rootRef={rootRef}>
       {error ? (
         <ErrorHint message={error} />
       ) : (
         <StyledVerticalStack full gap={4}>
           {routes?.buy.length && (
-            <StyledDataTable label={translate('general/links', 'Buy')}>
+            <StyledDataTable label={translate('screens/payment', 'Buy')}>
               {routes.buy.map<JSX.Element>((route) => (
                 <StyledDataTableExpandableRow
                   label={`${route.asset.blockchain} / ${route.asset.name}`}
                   expansionItems={[
                     { label: translate('screens/payment', 'Asset'), text: route.asset.name },
-                    { label: translate('screens/payment', 'Blockchain'), text: route.asset.blockchain },
+                    { label: translate('screens/home', 'Blockchain'), text: route.asset.blockchain },
                     { label: translate('screens/payment', 'Purpose of payment'), text: route.bankUsage },
                     {
-                      label: translate('screens/payment', 'Volume'),
+                      label: translate('screens/home', 'Volume'),
                       text: `${route.volume} CHF`,
                     },
                     {
@@ -127,7 +127,7 @@ export function PaymentRoutes(): JSX.Element {
             </StyledDataTable>
           )}
           {routes?.sell.length && (
-            <StyledDataTable label={translate('general/links', 'Sell')}>
+            <StyledDataTable label={translate('screens/payment', 'Sell')}>
               {routes.sell.map<JSX.Element>((route) => (
                 <StyledDataTableExpandableRow
                   label={`${route.currency.name} / ${route.iban}`}
@@ -148,13 +148,13 @@ export function PaymentRoutes(): JSX.Element {
             </StyledDataTable>
           )}
           {routes?.swap.length && (
-            <StyledDataTable label={translate('general/links', 'Swap')}>
+            <StyledDataTable label={translate('screens/payment', 'Swap')}>
               {routes.swap.map<JSX.Element>((route) => (
                 <StyledDataTableExpandableRow
                   label={route.asset.name}
                   expansionItems={[
                     { label: translate('screens/payment', 'Asset'), text: route.asset.name },
-                    { label: translate('screens/payment', 'Blockchain'), text: route.asset.blockchain },
+                    { label: translate('screens/home', 'Blockchain'), text: route.asset.blockchain },
                     {
                       label: translate('screens/payment', 'Deposit address'),
                       text: route.deposit.address,
