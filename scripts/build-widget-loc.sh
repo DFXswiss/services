576d8307e5e645b78174106fbd8f32e7a8cb85ad
David May <david.leo.may@gmail.com>
2024-07-26 10:33:12 +0200

    [NO-TASK] Added local widget build script

---
+++ scripts/build-widget-loc.sh
@@ -0,0 +1,8 @@
+version=v1.0
+
+npm run widget:loc
+mkdir -p ./build/widget/$version-chunks
+cp ./widget/static/js/main.*.js ./build/widget/$version
+cp ./widget/static/css/main.*.css ./build/widget/$version.css
+cp ./widget/$version-chunks/*.chunk.js ./build/widget/$version-chunks
+cp ./widget/$version-chunks/*.module.wasm ./build/widget/$version-chunks
