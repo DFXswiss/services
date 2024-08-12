168d81e46803046f51ab30121c873c61d312fd95
lapatric <42653152+lapatric@users.noreply.github.com>
2024-08-12 10:48:13 +0200

    [DEV-2972] reload on no pending payment error message

--- src/screens/payment-link.screen.tsx
+++ src/screens/payment-link.screen.tsx
@@ -18,6 +18,8 @@ import { Lnurl } from 'src/util/lnurl';
 import { blankedAddress } from 'src/util/utils';
 import { Layout } from '../components/layout';
 
+const noPaymentErrorMessage = 'No pending payment found';
+
 export default function PaymentLinkScreen(): JSX.Element {
   const { translate } = useSettingsContext();
   const { navigate } = useNavigation();
@@ -41,14 +43,23 @@ export default function PaymentLinkScreen(): JSX.Element {
       return;
     }
 
-    fetch(decodedUrl)
-      .then((response) => {
-        if (!response.ok) setError(`Error ${response.status}: ${response.statusText}`);
-        return response.json();
-      })
-      .then((data) => setLnurlPayRequest(data))
-      .catch((e) => setError((e as ApiError).message ?? 'Unknown error'));
+    fetchData(decodedUrl);
   }, [paramLNURL]);
+
+  async function fetchData(url: string) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) throw data;
+        setError(undefined);
+        setLnurlPayRequest(data);
+      })
+      .catch((e) => {
+        const errorMessage = (e as ApiError).message ?? 'Unknown error';
+        if (errorMessage === noPaymentErrorMessage) setTimeout(() => fetchData(url), 1000);
+        setError(errorMessage);
+      });
+  }
 
   const filteredTransferAmounts = lnurlPayRequest?.transferAmounts
     .filter(
@@ -63,7 +74,7 @@ export default function PaymentLinkScreen(): JSX.Element {
   return (
     <Layout backButton={false}>
       {error ? (
-        <ErrorHint message={error} />
+        <PaymentErrorHint message={error} />
       ) : !lnurlPayRequest || !paramLNURL ? (
         <StyledLoadingSpinner size={SpinnerSize.LG} />
       ) : (
@@ -102,3 +113,14 @@ export default function PaymentLinkScreen(): JSX.Element {
     </Layout>
   );
 }
+
+const PaymentErrorHint = ({ message }: { message: string }): JSX.Element => {
+  return message === noPaymentErrorMessage ? (
+    <>
+      <StyledLoadingSpinner size={SpinnerSize.LG} />
+      <p className="text-dfxGray-800 text-sm pt-3">{message}</p>
+    </>
+  ) : (
+    <ErrorHint message={message} />
+  );
+};
