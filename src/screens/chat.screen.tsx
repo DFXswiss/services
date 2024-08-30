cb15b7f44118723d9efd3defc1075ff1b1b00188
lapatric <42653152+lapatric@users.noreply.github.com>
2024-08-30 10:08:56 +0200

    [DEV-2825] refactor

--- src/screens/chat.screen.tsx
+++ src/screens/chat.screen.tsx
@@ -103,11 +103,7 @@ const initialChat: SupportIssue = {
 const emojiSet = ['👍', '❤️', '😂', '😮', '😢', '👏'];
 
 export default function ChatScreen(): JSX.Element {
-  const { translate } = useSettingsContext();
-
   const [chat, setChat] = useState<SupportIssue>(initialChat);
-  const [inputValue, setInputValue] = useState<string>('');
-  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
   const [clickedMessage, setClickedMessage] = useState<SupportMessage>();
   const [menuPosition, setMenuPosition] = useState<{ top: number; left: number }>({ top: 0, left: 0 });
   const [replyToMessage, setReplyToMessage] = useState<SupportMessage>();
@@ -119,61 +115,6 @@ export default function ChatScreen(): JSX.Element {
       messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
     }
   }, [chat.messages]);
-
-  const handleSend = () => {
-    const hasText = inputValue.trim() !== '';
-    const numFiles = selectedFiles.length;
-
-    if (!hasText && numFiles === 0) return;
-
-    const files = numFiles !== 1 && hasText ? [...selectedFiles, undefined] : selectedFiles;
-    files.forEach((file, index) => {
-      const newMessage: SupportMessage = {
-        id: (chat.messages.length + index + 1).toString(),
-        message: index === files.length - 1 ? inputValue : undefined,
-        media: file && {
-          name: file.name,
-          url: URL.createObjectURL(file), // Temporary URL for preview
-          type: file.type,
-          size: file.size,
-        },
-        date: new Date().toISOString(),
-        sender: 'user',
-        status: 'sent',
-        replyTo: index === 0 ? replyToMessage?.id : undefined,
-      };
-
-      setChat((prevChat) => ({
-        ...prevChat,
-        messages: [...prevChat.messages, newMessage],
-      }));
-    });
-
-    setInputValue('');
-    setSelectedFiles([]);
-    setReplyToMessage(undefined);
-    return;
-  };
-
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files;
-
-    if (files && files.length > 0) {
-      setSelectedFiles((prevFiles) => [...prevFiles, ...Array.from(files)]);
-      setTimeout(() => (e.target.value = ''), 100);
-    }
-  };
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      if (e.shiftKey) {
-        setInputValue((prevValue) => `${prevValue}\n`);
-      } else {
-        handleSend();
-      }
-    }
-  };
 
   const handleChatBubbleClick = (e?: React.MouseEvent<HTMLDivElement>, message?: SupportMessage) => {
     if (!e) {
@@ -237,145 +178,243 @@ export default function ChatScreen(): JSX.Element {
           })}
           <div ref={messagesEndRef} />
         </div>
-
-        <div className="flex flex-col gap-2 p-4 bg-dfxGray-300">
-          {replyToMessage && (
-            <div className="flex flex-row bg-dfxGray-800/10 rounded-md overflow-clip mx-1.5 py-1 text-dfxBlue-800">
-              <div className="w-1 h-full bg-dfxBlue-800" />
-              <div className="flex flex-row flex-grow px-2 py-1">
-                <img
-                  src={replyToMessage.media?.url}
-                  alt="Media"
-                  className="rounded-sm max-h-10 object-cover"
-                  style={{ maxWidth: '100%', width: 'auto', height: 'auto' }}
-                />
-                <div className="flex flex-col px-2 text-left">
-                  <div className="flex flex-row">
-                    <BsReply className="text-dfxBlue-800 text-lg mr-1" />
-                    <p className="font-semibold text-sm">{`Reply to ${replyToMessage.sender}`}</p>
-                  </div>
-                  <p className="text-sm line-clamp-1 overflow-ellipsis">{replyToMessage.message ?? 'Media'}</p>
-                </div>
-              </div>
-              <button className="px-5 cursor-pointer text-dfxGray-700" onClick={() => setReplyToMessage(undefined)}>
-                <MdOutlineCancel className="text-xl" />
-              </button>
-            </div>
-          )}
-          {selectedFiles.length > 0 && (
-            <div className="flex flex-row flex-wrap gap-2">
-              {selectedFiles.map((file, index) => (
-                <div key={index} className="flex flex-row gap-1.5 items-center bg-dfxGray-800/20 rounded-md p-2 pr-3">
-                  <HiOutlinePaperClip className="text-dfxGray-800 text-lg" />
-                  <p className="text-dfxGray-800 text-left text-sm">
-                    {blankedAddress(file.name, { displayLength: 20 })}
-                  </p>
-                </div>
-              ))}
-            </div>
-          )}
-          <div className="flex flex-row items-center space-x-2">
-            <label className="flex items-center p-2 cursor-pointer">
-              <HiOutlinePaperClip className="text-2xl text-dfxGray-800" />
-              <input type="file" multiple onChange={handleFileChange} className="hidden" />
-            </label>
-
-            <div
-              className="
-                grid
-                w-full
-                text-sm
-                after:px-3.5
-                after:py-2.5
-                [&>textarea]:text-inherit
-                after:text-inherit
-                [&>textarea]:resize-none
-                [&>textarea]:overflow-hidden
-                [&>textarea]:[grid-area:1/1/2/2]
-                after:[grid-area:1/1/2/2]
-                after:whitespace-pre-wrap
-                after:invisible
-                after:content-[attr(data-cloned-val)_'_']
-                after:border
-                text-dfxGray-800
-                outline-none
-                resize-none
-                overflow-auto
-                max-h-40"
-              data-cloned-val={inputValue}
-            >
-              <textarea
-                className="
-                  w-full
-                  text-slate-600
-                  bg-dfxGray-300
-                  appearance-none
-                  rounded
-                  px-3.5
-                  py-2.5
-                  outline-none"
-                name="message"
-                id="message"
-                rows={1}
-                value={inputValue}
-                onInput={(e) => setInputValue(e.currentTarget.value)}
-                onKeyDown={handleKeyDown}
-                placeholder="Write a message..."
-                required
-              />
-            </div>
-
-            <button onClick={handleSend} className="items-center p-2 cursor-pointer">
-              <MdSend className="text-2xl text-dfxBlue-800" />
-            </button>
-          </div>
-        </div>
+        <InputComponent
+          chat={chat}
+          setChat={setChat}
+          replyToMessage={replyToMessage}
+          setReplyToMessage={setReplyToMessage}
+        />
         {clickedMessage && (
-          <div
-            style={{
-              top: menuPosition.top,
-              left: menuPosition.left,
-              transform: `${menuPosition.left > window.innerWidth / 2 ? 'translateX(-100%)' : 'translateX(0)'}
-                ${menuPosition.top > window.innerHeight / 2 ? 'translateY(-100%)' : 'translateY(0)'}`,
+          <ChatBubbleMenu
+            menuPosition={menuPosition}
+            clickedMessage={clickedMessage}
+            setReplyToMessage={(message) => {
+              setReplyToMessage(message);
+              setClickedMessage(undefined);
             }}
-            className="absolute pointer-events-none"
-          >
-            <div className="flex flex-row border border-dfxGray-400 shadow-md z-10 bg-white rounded-full overflow-clip h-10 mb-1 pointer-events-auto">
-              {emojiSet.map((emoji, index) => (
-                <button
-                  key={index}
-                  className="hover:bg-dfxGray-300 w-10 text-lg flex items-center justify-center"
-                  onClick={() => handleEmojiClick(clickedMessage.id, emoji)}
-                >
-                  {emoji}
-                </button>
-              ))}
-            </div>
-            <div className="w-36 border border-dfxGray-400 shadow-md z-10 bg-white rounded-md overflow-clip text-dfxBlue-800 pointer-events-auto">
-              <div className="flex flex-col divide-y-0.5 divide-dfxGray-400 items-start bg-dfxGray-100 w-36">
-                <button
-                  className="hover:bg-dfxGray-300 w-full text-left px-4 py-2"
-                  onClick={() => {
-                    console.log('Copy');
-                  }}
-                >
-                  {translate('general/actions', 'Copy')}
-                </button>
-                <button
-                  className="hover:bg-dfxGray-300 w-full text-left px-4 py-2"
-                  onClick={() => {
-                    setReplyToMessage(clickedMessage);
-                    setClickedMessage(undefined);
-                  }}
-                >
-                  {translate('general/actions', 'Reply')}
-                </button>
-              </div>
-            </div>
-          </div>
+            handleEmojiClick={handleEmojiClick}
+          />
         )}
       </div>
     </Layout>
+  );
+}
+
+interface ChatBubbleMenuProps {
+  menuPosition: { top: number; left: number };
+  clickedMessage: SupportMessage;
+  setReplyToMessage: React.Dispatch<React.SetStateAction<SupportMessage | undefined>>;
+  handleEmojiClick: (messageId: string, emoji: string) => void;
+}
+
+function ChatBubbleMenu({
+  menuPosition,
+  clickedMessage,
+  setReplyToMessage,
+  handleEmojiClick,
+}: ChatBubbleMenuProps): JSX.Element {
+  const { translate } = useSettingsContext();
+
+  return (
+    <div
+      style={{
+        top: menuPosition.top,
+        left: menuPosition.left,
+        transform: `${menuPosition.left > window.innerWidth / 2 ? 'translateX(-100%)' : 'translateX(0)'}
+        ${menuPosition.top > window.innerHeight / 2 ? 'translateY(-100%)' : 'translateY(0)'}`,
+      }}
+      className="absolute pointer-events-none"
+    >
+      <div className="flex flex-row border border-dfxGray-400 shadow-md z-10 bg-white rounded-full overflow-clip h-10 mb-1 pointer-events-auto">
+        {emojiSet.map((emoji, index) => (
+          <button
+            key={index}
+            className="hover:bg-dfxGray-300 w-10 text-lg flex items-center justify-center"
+            onClick={() => handleEmojiClick(clickedMessage.id, emoji)}
+          >
+            {emoji}
+          </button>
+        ))}
+      </div>
+      <div className="w-36 border border-dfxGray-400 shadow-md z-10 bg-white rounded-md overflow-clip text-dfxBlue-800 pointer-events-auto">
+        <div className="flex flex-col divide-y-0.5 divide-dfxGray-400 items-start bg-dfxGray-100 w-36">
+          <button
+            className="hover:bg-dfxGray-300 w-full text-left px-4 py-2"
+            onClick={() => {
+              console.log('Copy');
+            }}
+          >
+            {translate('general/actions', 'Copy')}
+          </button>
+          <button
+            className="hover:bg-dfxGray-300 w-full text-left px-4 py-2"
+            onClick={() => setReplyToMessage(clickedMessage)}
+          >
+            {translate('general/actions', 'Reply')}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+interface InputComponentProps {
+  chat: SupportIssue;
+  setChat: React.Dispatch<React.SetStateAction<SupportIssue>>;
+  replyToMessage?: SupportMessage;
+  setReplyToMessage: React.Dispatch<React.SetStateAction<SupportMessage | undefined>>;
+}
+
+function InputComponent({ chat, setChat, replyToMessage, setReplyToMessage }: InputComponentProps): JSX.Element {
+  const [inputValue, setInputValue] = useState<string>('');
+  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+
+  const handleSend = () => {
+    const hasText = inputValue.trim() !== '';
+    const numFiles = selectedFiles.length;
+
+    if (!hasText && numFiles === 0) return;
+
+    const files = numFiles !== 1 && hasText ? [...selectedFiles, undefined] : selectedFiles;
+    files.forEach((file, index) => {
+      const newMessage: SupportMessage = {
+        id: (chat.messages.length + index + 1).toString(),
+        message: index === files.length - 1 ? inputValue : undefined,
+        media: file && {
+          name: file.name,
+          url: URL.createObjectURL(file),
+          type: file.type,
+          size: file.size,
+        },
+        date: new Date().toISOString(),
+        sender: 'user',
+        status: 'sent',
+        replyTo: index === 0 ? replyToMessage?.id : undefined,
+      };
+
+      setChat((prevChat) => ({
+        ...prevChat,
+        messages: [...prevChat.messages, newMessage],
+      }));
+    });
+
+    setInputValue('');
+    setSelectedFiles([]);
+    setReplyToMessage(undefined);
+    return;
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+
+    if (files && files.length > 0) {
+      setSelectedFiles((prevFiles) => [...prevFiles, ...Array.from(files)]);
+      setTimeout(() => (e.target.value = ''), 100);
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      if (e.shiftKey) {
+        setInputValue((prevValue) => `${prevValue}\n`);
+      } else {
+        handleSend();
+      }
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-2 p-4 bg-dfxGray-300">
+      {replyToMessage && (
+        <div className="flex flex-row bg-dfxGray-800/10 rounded-md overflow-clip mx-1.5 py-1 text-dfxBlue-800">
+          <div className="w-1 h-full bg-dfxBlue-800" />
+          <div className="flex flex-row flex-grow px-2 py-1">
+            <img
+              src={replyToMessage.media?.url}
+              alt="Media"
+              className="rounded-sm max-h-10 object-cover"
+              style={{ maxWidth: '100%', width: 'auto', height: 'auto' }}
+            />
+            <div className="flex flex-col px-2 text-left">
+              <div className="flex flex-row">
+                <BsReply className="text-dfxBlue-800 text-lg mr-1" />
+                <p className="font-semibold text-sm">{`Reply to ${replyToMessage.sender}`}</p>
+              </div>
+              <p className="text-sm line-clamp-1 overflow-ellipsis">{replyToMessage.message ?? 'Media'}</p>
+            </div>
+          </div>
+          <button className="px-5 cursor-pointer text-dfxGray-700" onClick={() => setReplyToMessage(undefined)}>
+            <MdOutlineCancel className="text-xl" />
+          </button>
+        </div>
+      )}
+      {selectedFiles.length > 0 && (
+        <div className="flex flex-row flex-wrap gap-2">
+          {selectedFiles.map((file, index) => (
+            <div key={index} className="flex flex-row gap-1.5 items-center bg-dfxGray-800/20 rounded-md p-2 pr-3">
+              <HiOutlinePaperClip className="text-dfxGray-800 text-lg" />
+              <p className="text-dfxGray-800 text-left text-sm">{blankedAddress(file.name, { displayLength: 20 })}</p>
+            </div>
+          ))}
+        </div>
+      )}
+      <div className="flex flex-row items-center space-x-2">
+        <label className="flex items-center p-2 cursor-pointer">
+          <HiOutlinePaperClip className="text-2xl text-dfxGray-800" />
+          <input type="file" multiple onChange={handleFileChange} className="hidden" />
+        </label>
+
+        <div
+          className="
+          grid
+          w-full
+          text-sm
+          after:px-3.5
+          after:py-2.5
+          [&>textarea]:text-inherit
+          after:text-inherit
+          [&>textarea]:resize-none
+          [&>textarea]:overflow-hidden
+          [&>textarea]:[grid-area:1/1/2/2]
+          after:[grid-area:1/1/2/2]
+          after:whitespace-pre-wrap
+          after:invisible
+          after:content-[attr(data-cloned-val)_'_']
+          after:border
+          text-dfxGray-800
+          outline-none
+          resize-none
+          overflow-auto
+          max-h-40"
+          data-cloned-val={inputValue}
+        >
+          <textarea
+            className="
+            w-full
+            text-slate-600
+            bg-dfxGray-300
+            appearance-none
+            rounded
+            px-3.5
+            py-2.5
+            outline-none"
+            name="message"
+            id="message"
+            rows={1}
+            value={inputValue}
+            onInput={(e) => setInputValue(e.currentTarget.value)}
+            onKeyDown={handleKeyDown}
+            placeholder="Write a message..."
+            required
+          />
+        </div>
+
+        <button onClick={handleSend} className="items-center p-2 cursor-pointer">
+          <MdSend className="text-2xl text-dfxBlue-800" />
+        </button>
+      </div>
+    </div>
   );
 }
 
