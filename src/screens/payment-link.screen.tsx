990e8f2d8ccd39719569050731a5e7562242f6e8
lapatric <42653152+lapatric@users.noreply.github.com>
2024-09-06 12:17:32 +0200

    [DEV-3090] small improvement

--- src/screens/payment-link.screen.tsx
+++ src/screens/payment-link.screen.tsx
@@ -18,9 +18,9 @@ import { useForm, useWatch } from 'react-hook-form';
 import { useSearchParams } from 'react-router-dom';
 import { ErrorHint } from 'src/components/error-hint';
 import { QrBasic } from 'src/components/payment/qr-code';
-import { useAppHandlingContext } from 'src/contexts/app-handling.context';
 import { useSettingsContext } from 'src/contexts/settings.context';
 import { useWindowContext } from 'src/contexts/window.context';
+import { useAppParams } from 'src/hooks/app-params.hook';
 import { useNavigation } from 'src/hooks/navigation.hook';
 import { Lnurl } from 'src/util/lnurl';
 import { blankedAddress, formatLocationAddress, url } from 'src/util/utils';
@@ -239,8 +239,8 @@ const recommendedWallets = ['Frankencoin', 'Cake Wallet', 'Wallet of Satoshi', '
 
 export default function PaymentLinkScreen(): JSX.Element {
   const { translate } = useSettingsContext();
-  const { params } = useAppHandlingContext();
   const { navigate } = useNavigation();
+  const { lightning } = useAppParams();
   const { width } = useWindowContext();
 
   const [urlParams, setUrlParams] = useSearchParams();
@@ -270,7 +270,7 @@ export default function PaymentLinkScreen(): JSX.Element {
   const selectedEthereumUriAsset = useWatch({ control, name: 'asset' });
 
   useEffect(() => {
-    const lightningParam = params?.lightning;
+    const lightningParam = lightning;
 
     let apiUrl: string | undefined;
     if (lightningParam) {
