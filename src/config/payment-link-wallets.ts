f4dda450db26108816f3d310d8f2ce2e6b346a5c
Noah <noah@dfx.swiss>
2024-09-25 11:49:01 +0200

    Aqua Wallet hinzugefügt (#434)

--- src/config/payment-link-wallets.ts
+++ src/config/payment-link-wallets.ts
@@ -85,9 +85,9 @@ export const CompatibleWallets: { [key: string]: { websiteUrl: string; iconUrl: 
     websiteUrl: 'https://lnbits.com/',
     iconUrl: 'https://content.dfx.swiss/img/v1/services/wallets/LNbits.webp',
   },
-  Muun: {
-    websiteUrl: 'https://muun.com/',
-    iconUrl: 'https://content.dfx.swiss/img/v1/services/wallets/muun.webp',
+  AQUA: {
+    websiteUrl: 'https://aquawallet.io/',
+    iconUrl: 'https://content.dfx.swiss/img/v1/services/wallets/aqua.webp',
   },
   OneKey: {
     websiteUrl: 'https://onekey.so/',
