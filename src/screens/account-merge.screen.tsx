99b4e06b01de61d3aef5013c908a3712ff2e9481
lapatric <42653152+lapatric@users.noreply.github.com>
2024-10-22 15:51:16 +0200

    [DEV-3094] improve user feedback in account-merge screen

--- src/screens/account-merge.screen.tsx
+++ src/screens/account-merge.screen.tsx
@@ -1,6 +1,6 @@
 import { ApiError, useApi, useAuthContext } from '@dfx.swiss/react';
-import { SpinnerSize, StyledLoadingSpinner, StyledVerticalStack } from '@dfx.swiss/react-components';
-import { useEffect } from 'react';
+import { SpinnerSize, StyledButton, StyledLoadingSpinner, StyledVerticalStack } from '@dfx.swiss/react-components';
+import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { Layout } from 'src/components/layout';
 import { useSettingsContext } from 'src/contexts/settings.context';
@@ -18,37 +18,61 @@ export default function AccountMerge() {
   const { call } = useApi();
 
   const [urlParams, setUrlParams] = useSearchParams();
+  const [kycHash, setKycHash] = useState<string>();
 
   const otp = urlParams.get('otp');
 
   useEffect(() => {
     if (otp) {
+      urlParams.delete('otp');
+      setUrlParams(urlParams);
+
       call<MergeRedirect>({
         url: `auth/mail/confirm?code=${otp}`,
         method: 'GET',
       })
         .then(({ kycHash, accessToken }: MergeRedirect) => {
           setAuthenticationToken(accessToken);
-          navigate(`/kyc?code=${kycHash}`);
+          setKycHash(kycHash);
         })
         .catch((error: ApiError) => {
-          navigate(`/error?msg=${error.message}`);
+          const errorMessage =
+            error.statusCode === 400
+              ? translate('screens/error', 'Invalid link')
+              : error.statusCode === 409
+              ? translate('screens/error', 'Merge is already completed')
+              : error.message;
+
+          navigate({ pathname: '/error', search: `msg=${errorMessage}` });
         });
     } else {
       navigate('/kyc');
-    }
-
-    if (urlParams.has('otp')) {
-      urlParams.delete('otp');
-      setUrlParams(urlParams);
     }
   }, []);
 
   return (
     <Layout>
-      <StyledVerticalStack gap={6} full center>
-        <StyledLoadingSpinner size={SpinnerSize.LG} />
-        <p className="text-dfxGray-700">{translate('screens/kyc', 'Merging your accounts...')} </p>
+      <StyledVerticalStack center gap={5} marginY={5}>
+        {kycHash ? (
+          <>
+            <div>
+              <h2 className="text-dfxBlue-800">{translate('screens/error', 'Account merged successfully ')}</h2>
+              <p className="text-dfxGray-700">
+                {translate('screens/error', 'You can now navigate to your account screen.')}
+              </p>
+            </div>
+
+            <StyledButton
+              label={translate('screens/kyc', 'My account')}
+              onClick={() => navigate(`/account?code=${kycHash}`)}
+            />
+          </>
+        ) : (
+          <>
+            <StyledLoadingSpinner size={SpinnerSize.LG} />
+            <p className="text-dfxGray-700">{translate('screens/kyc', 'Merging your accounts...')} </p>
+          </>
+        )}
       </StyledVerticalStack>
     </Layout>
   );
