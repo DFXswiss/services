035579c1907838d1de476383d7d481fb932e92bb
lapatric <42653152+lapatric@users.noreply.github.com>
2024-11-13 11:15:40 +0100

    [DEV-3354] auto-update services on new version release

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,24 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<Main />);
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register(`${process.env.PUBLIC_URL}/custom-service-worker.js`)
+    .then((registration) => {
+      registration.onupdatefound = () => {
+        const newWorker = registration.installing;
+        if (newWorker) {
+          newWorker.onstatechange = () => {
+            if (newWorker.state === 'activated' && navigator.serviceWorker.controller) {
+              window.location.reload();
+            }
+          };
+        }
+      };
+    })
+    .catch((error) => console.error('Service worker registration failed:', error));
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
