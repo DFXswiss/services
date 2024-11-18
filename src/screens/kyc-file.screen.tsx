ca83d4ff8c14f98e0d1adabb10494c30e5778cd1
Patrick <42653152+lapatric@users.noreply.github.com>
2024-11-18 13:55:19 +0100

    [DEV-3081] Add screen for KYC file previews (#484)
    * [DEV-3081] add screen for KYC file previews
    
    * [DEV-3081] complete file preview
    
    * [DEV-3081] minor fixes + cleanup
    
    * [DEV-3081] improvements

---
+++ src/screens/kyc-file.screen.tsx
@@ -0,0 +1,115 @@
+import { ApiError, useApi } from '@dfx.swiss/react';
+import {
+  AlignContent,
+  SpinnerSize,
+  StyledButton,
+  StyledButtonWidth,
+  StyledDataTable,
+  StyledDataTableRow,
+  StyledLoadingSpinner,
+  StyledVerticalStack,
+} from '@dfx.swiss/react-components';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { ErrorHint } from 'src/components/error-hint';
+import { Layout } from 'src/components/layout';
+import { FileType, FileTypeLabels } from 'src/config/labels';
+import { useSettingsContext } from 'src/contexts/settings.context';
+import { openImageFromString, openPdfFromString } from 'src/util/utils';
+
+// TODO: Add to packages
+interface KycFileData {
+  uid: string;
+  name: string;
+  type: FileType;
+  contentType: string;
+  content: any;
+}
+
+export default function KycFileScreen(): JSX.Element {
+  const { call } = useApi();
+  const { translate } = useSettingsContext();
+  const { id: kycFileId } = useParams();
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string>();
+  const [file, setFile] = useState<KycFileData>();
+
+  useEffect(() => {
+    if (kycFileId) {
+      setIsLoading(true);
+      call<any>({
+        url: `kyc/file/${kycFileId}`,
+        version: 'v2',
+        method: 'GET',
+      })
+        .then(setFile)
+        .catch((e: ApiError) => {
+          setError(e.message ?? 'Unknown error');
+        })
+        .finally(() => setIsLoading(false));
+    } else {
+      setError('No key provided');
+    }
+  }, [kycFileId]);
+
+  return (
+    <Layout title={translate('screens/kyc', 'KYC file')}>
+      {error ? (
+        <ErrorHint message={error} />
+      ) : isLoading || !file ? (
+        <StyledLoadingSpinner size={SpinnerSize.LG} />
+      ) : (
+        <FilePreview file={file} setErrorMessage={setError} />
+      )}
+    </Layout>
+  );
+}
+
+interface FilePreviewProps {
+  file: KycFileData;
+  setErrorMessage: (message: string) => void;
+}
+
+function FilePreview({ file, setErrorMessage }: FilePreviewProps): JSX.Element {
+  const { translate } = useSettingsContext();
+
+  const { content, contentType } = file;
+  const [fileType] = contentType.split('/');
+
+  if (!content || content.type !== 'Buffer' || !Array.isArray(content.data)) {
+    setErrorMessage('Invalid file type');
+    return <></>;
+  }
+
+  const base64Data = Buffer.from(content.data).toString('base64');
+
+  const handleOpenFile = () => {
+    if (fileType === 'application') {
+      openPdfFromString(base64Data);
+    } else if (fileType === 'image') {
+      openImageFromString(base64Data, contentType);
+    }
+  };
+
+  return (
+    <StyledVerticalStack full center gap={2}>
+      <StyledDataTable alignContent={AlignContent.RIGHT} showBorder minWidth={false}>
+        <StyledDataTableRow label={translate('screens/payment', 'ID')}>
+          <p>{file.uid}</p>
+        </StyledDataTableRow>
+        <StyledDataTableRow label={translate('screens/payment', 'Name')}>
+          <p>{file.name}</p>
+        </StyledDataTableRow>
+        <StyledDataTableRow label={translate('screens/payment', 'Type')}>
+          <p>{translate('screens/kyc', FileTypeLabels[file.type])}</p>
+        </StyledDataTableRow>
+      </StyledDataTable>
+      <StyledButton
+        width={StyledButtonWidth.FULL}
+        onClick={handleOpenFile}
+        label={translate('general/actions', 'View file')}
+      ></StyledButton>
+    </StyledVerticalStack>
+  );
+}
