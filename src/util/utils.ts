c732733d80da33f4a7e006382146e6c3a624080a
lapatric <42653152+lapatric@users.noreply.github.com>
2024-12-04 14:02:53 +0100

    [DEV-3387] simplify

--- src/util/utils.ts
+++ src/util/utils.ts
@@ -154,15 +154,6 @@ export function formatUnits(value: string, decimals = 18): string {
 }
 
 export function generateExportFileName(): string {
-  const now = new Date();
-
-  const year = now.getFullYear();
-  const month = String(now.getMonth() + 1).padStart(2, '0');
-  const day = String(now.getDate()).padStart(2, '0');
-
-  const hours = String(now.getHours()).padStart(2, '0');
-  const minutes = String(now.getMinutes()).padStart(2, '0');
-  const seconds = String(now.getSeconds()).padStart(2, '0');
-
-  return `DFX_export_${year}${month}${day}_${hours}${minutes}${seconds}.zip`;
+  const [date, time] = new Date().toISOString().replace(/[-:]/g, '').split(/[T\.]/);
+  return `DFX_export_${date}_${time}.zip`;
 }
