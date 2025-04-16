1b2077825422637ac43f685d07de81bc7372bbb5
lapatric <42653152+lapatric@users.noreply.github.com>
2025-04-16 18:01:43 +0200

    [DEV-3794] invoice for buy w/ EUR

--- src/components/payment/payment-qr-code.tsx
+++ src/components/payment/payment-qr-code.tsx
@@ -42,30 +42,36 @@ export function PaymentQrCode({ value, txId }: GiroCodeProps): JSX.Element {
     }
   }
 
-  return stringIsSVG(value) ? (
-    <div className="flex flex-col items-center py-4 gap-1.5">
-      <img
-        className="mx-auto h-auto w-full max-w-[15rem]"
-        src={`data:image/svg+xml;utf8,${encodeURIComponent(value)}`}
-      />
-      <p className="text-dfxBlue-800 font-semibold text-base">{translate('screens/buy', 'QR-bill')}</p>
-      <button
-        type="button"
-        onClick={onInvoiceClick}
-        disabled={isLoading}
-        className="flex flex-row rounded-md px-2.5 py-1.5 items-center gap-1.5 text-dfxBlue-800 font-semibold text-sm cursor-pointer bg-dfxGray-400 hover:bg-dfxGray-500 disabled:bg-dfxGray-400 disabled:cursor-default disabled:text-dfxGray-600 disabled:hover:bg-dfxGray-400"
-      >
-        <>
-          {isLoading && <StyledLoadingSpinner variant={SpinnerVariant.LIGHT_MODE} size={SpinnerSize.MD} />}
-          {translate('screens/buy', 'PDF Invoice')}
-          <RiExternalLinkFill className="-ml-0.5 text-base" />
-        </>
-      </button>
-    </div>
-  ) : (
-    <div className="flex flex-col items-center py-4 gap-1">
-      <QRCode className="mx-auto h-auto w-full max-w-[15rem]" value={value} fgColor={'#072440'} />
-      <p className="text-dfxBlue-800 font-semibold text-base">GiroCode</p>
-    </div>
+  return (
+    <>
+      {stringIsSVG(value) ? (
+        <div className="flex flex-col items-center py-4 gap-1.5">
+          <img
+            className="mx-auto h-auto w-full max-w-[15rem]"
+            src={`data:image/svg+xml;utf8,${encodeURIComponent(value)}`}
+          />
+          <p className="text-dfxBlue-800 font-semibold text-base">{translate('screens/buy', 'QR-bill')}</p>
+        </div>
+      ) : (
+        <div className="flex flex-col items-center py-4 gap-1">
+          <QRCode className="mx-auto h-auto w-full max-w-[15rem]" value={value} fgColor={'#072440'} />
+          <p className="text-dfxBlue-800 font-semibold text-base">GiroCode</p>
+        </div>
+      )}
+      <div className="flex flex-col items-center gap-1.5">
+        <button
+          type="button"
+          onClick={onInvoiceClick}
+          disabled={isLoading}
+          className="flex flex-row rounded-md px-2.5 py-1.5 items-center gap-1.5 text-dfxBlue-800 font-semibold text-sm cursor-pointer bg-dfxGray-400 hover:bg-dfxGray-500 disabled:bg-dfxGray-400 disabled:cursor-default disabled:text-dfxGray-600 disabled:hover:bg-dfxGray-400"
+        >
+          <>
+            {isLoading && <StyledLoadingSpinner variant={SpinnerVariant.LIGHT_MODE} size={SpinnerSize.MD} />}
+            {translate('screens/buy', 'PDF Invoice')}
+            <RiExternalLinkFill className="-ml-0.5 text-base" />
+          </>
+        </button>
+      </div>
+    </>
   );
 }
