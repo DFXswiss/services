3e22055911faf0d2162b48991e73c1990e64d555
lapatric <42653152+lapatric@users.noreply.github.com>
2025-05-09 15:50:20 +0200

    [DEV-3888] add chart & currency switcher

---
+++ src/components/safe/chart.tsx
@@ -0,0 +1,138 @@
+import { SpinnerSize, StyledLoadingSpinner } from '@dfx.swiss/react-components';
+import { useMemo, useState } from 'react';
+import Chart from 'react-apexcharts';
+import { useSettingsContext } from 'src/contexts/settings.context';
+import { CustodyHistoryEntry, FiatCurrency } from 'src/hooks/safe.hook';
+import { ButtonGroup } from './button-group';
+
+enum Timeframe {
+  WEEK = '1W',
+  MONTH = '1M',
+  QUARTER = '1Q',
+  YEAR = '1Y',
+  ALL = 'All',
+}
+
+const getFromDateByTimeframe = (timeframe: Timeframe) => {
+  switch (timeframe) {
+    case Timeframe.ALL:
+      return 0;
+    case Timeframe.WEEK:
+      return Date.now() - 7 * 24 * 60 * 60 * 1000;
+    case Timeframe.MONTH:
+      return Date.now() - 30 * 24 * 60 * 60 * 1000;
+    case Timeframe.QUARTER:
+      return Date.now() - 90 * 24 * 60 * 60 * 1000;
+    case Timeframe.YEAR:
+      return Date.now() - 365 * 24 * 60 * 60 * 1000;
+    default:
+      return 0;
+  }
+};
+
+interface PriceChartProps {
+  isLoading: boolean;
+  history: CustodyHistoryEntry[];
+  currency: FiatCurrency;
+}
+
+export const PriceChart = ({ isLoading, history, currency }: PriceChartProps) => {
+  const { translate } = useSettingsContext();
+
+  const [timeframe, setTimeframe] = useState<Timeframe>(Timeframe.ALL);
+
+  const filteredHistory = useMemo(
+    () => history.filter((entry) => new Date(entry.date).getTime() > getFromDateByTimeframe(timeframe)),
+    [history, timeframe],
+  );
+
+  const maxPrice = useMemo(
+    () => filteredHistory.reduce((max, entry) => Math.max(max, entry.value[currency]), 0),
+    [filteredHistory],
+  );
+
+  return isLoading ? (
+    <div className="leading-none">
+      <StyledLoadingSpinner size={SpinnerSize.MD} />
+    </div>
+  ) : (
+    <div id="chart-timeline" className="relative text-dfxBlue-500">
+      <Chart
+        type="area"
+        height={250}
+        options={{
+          theme: {
+            monochrome: {
+              color: '#092f62',
+              enabled: true,
+            },
+          },
+          chart: {
+            type: 'area',
+            dropShadow: {
+              enabled: false,
+            },
+            toolbar: {
+              show: false,
+            },
+            zoom: {
+              enabled: false,
+            },
+            background: '0',
+          },
+          stroke: {
+            width: 3,
+          },
+          dataLabels: {
+            enabled: false,
+          },
+          grid: {
+            show: false,
+          },
+          xaxis: {
+            type: 'datetime',
+            labels: {
+              show: false,
+            },
+            axisBorder: {
+              show: false,
+            },
+            axisTicks: {
+              show: false,
+            },
+          },
+          yaxis: {
+            show: false,
+            min: 0,
+            max: maxPrice,
+          },
+          fill: {
+            colors: ['#5A81BB'],
+            type: 'gradient',
+            gradient: {
+              type: 'vertical',
+              opacityFrom: 1,
+              opacityTo: 0.0,
+            },
+          },
+        }}
+        series={[
+          {
+            name: translate('screens/safe', 'Portfolio value'),
+            data: filteredHistory.map((entry: CustodyHistoryEntry) => {
+              return [new Date(entry.date).getTime(), entry.value[currency]];
+            }),
+          },
+        ]}
+      />
+      <div className="absolute bottom-2.5 w-full flex justify-center py-2">
+        <ButtonGroup<Timeframe>
+          items={Object.values(Timeframe)}
+          selected={timeframe}
+          onClick={(t) => setTimeframe(t)}
+          buttonLabel={(t) => t}
+        />
+      </div>
+    </div>
+  );
+};
--- src/screens/safe.screen.tsx
+++ src/screens/safe.screen.tsx
@@ -1,28 +1,24 @@
-import {
-  AlignContent,
-  AssetIconSize,
-  AssetIconVariant,
-  DfxAssetIcon,
-  SpinnerSize,
-  StyledDataTable,
-  StyledDataTableRow,
-  StyledLoadingSpinner,
-  StyledVerticalStack,
-} from '@dfx.swiss/react-components';
-import { useRef } from 'react';
+import { SpinnerSize, StyledLoadingSpinner } from '@dfx.swiss/react-components';
+import { useRef, useState } from 'react';
 import { ErrorHint } from 'src/components/error-hint';
+import { ButtonGroup } from 'src/components/safe/button-group';
+import { PriceChart } from 'src/components/safe/chart';
+import { Portfolio } from 'src/components/safe/portfolio';
 import { useSettingsContext } from 'src/contexts/settings.context';
 import { useUserGuard } from 'src/hooks/guard.hook';
-import { CustodyAssetBalance, FiatCurrency, useSafe } from 'src/hooks/safe.hook';
+import { FiatCurrency, useSafe } from 'src/hooks/safe.hook';
 import { formatCurrency } from 'src/util/utils';
 import { Layout } from '../components/layout';
 
 export default function SafeScreen(): JSX.Element {
   useUserGuard('/login');
 
+  const { isInitialized, totalValue, portfolio, history, isLoadingPortfolio, isLoadingHistory, error } = useSafe();
   const { translate } = useSettingsContext();
-  const { error, isInitialized, isLoading, currency, portfolio, totalValue } = useSafe();
+
   const rootRef = useRef<HTMLDivElement>(null);
+
+  const [currency, setCurrency] = useState<FiatCurrency>(FiatCurrency.CHF);
 
   return (
     <Layout rootRef={rootRef} title={translate('screens/safe', 'My DFX Safe')}>
@@ -33,7 +29,7 @@ export default function SafeScreen(): JSX.Element {
       ) : !isInitialized ? (
         <StyledLoadingSpinner size={SpinnerSize.LG} />
       ) : (
-        <div className="flex flex-col w-full gap-2">
+        <div className="flex flex-col w-full gap-4">
           <div className="shadow-card rounded-xl">
             <div id="chart-timeline" className="relative">
               <div className="p-2 gap-2 flex flex-col items-start">
@@ -41,76 +37,39 @@ export default function SafeScreen(): JSX.Element {
                   <h2 className="text-dfxBlue-800 text-left">{translate('screens/safe', 'Portfolio')}</h2>
                   <p className="text-dfxGray-700 text-left">{translate('screens/safe', 'Total portfolio value')}</p>
                 </div>
-                <div className="flex flex-row items-center gap-2">
-                  {isLoading ? (
-                    <div className="leading-none">
-                      <StyledLoadingSpinner size={SpinnerSize.MD} />
+                {isLoadingPortfolio ? (
+                  <div className="leading-none">
+                    <StyledLoadingSpinner size={SpinnerSize.MD} />
+                  </div>
+                ) : (
+                  <div className="relative w-full h-full">
+                    <div className="flex flex-row items-center gap-3">
+                      <ButtonGroup<FiatCurrency>
+                        items={Object.values(FiatCurrency)}
+                        selected={currency}
+                        onClick={(_currency) => setCurrency(_currency)}
+                        buttonLabel={(_currency) => _currency.toUpperCase()}
+                        size={'sm'}
+                      />
+                      <div className="text-dfxBlue-800">
+                        <span className="text-base font-bold leading-tight">
+                          {formatCurrency(totalValue[currency], 2, 2)}
+                        </span>{' '}
+                        <span className="text-base font-[350] leading-tight">{currency.toUpperCase()}</span>
+                      </div>
                     </div>
-                  ) : (
-                    <div className="text-dfxBlue-800">
-                      <span className="text-lg font-bold leading-tight">{formatCurrency(totalValue, 2, 2)}</span>{' '}
-                      <span className="text-base font-[350] leading-tight">{currency}</span>
-                    </div>
-                  )}
-                </div>
+                    <PriceChart isLoading={isLoadingHistory} history={history} currency={currency} />
+                  </div>
+                )}
               </div>
             </div>
           </div>
-          <Portfolio portfolio={portfolio} currency={currency} isLoading={isLoading} />
+          <div className="px-2 text-dfxBlue-500 text-left text-lg font-semibold">
+            {translate('screens/safe', 'Assets')}
+          </div>
+          <Portfolio portfolio={portfolio} currency={currency} isLoading={isLoadingPortfolio} />
         </div>
       )}
     </Layout>
   );
 }
-
-/**
- * ***********************************************
- *               PORTFOLIO COMPONENT
- * ***********************************************
- */
-
-interface PortfolioProps {
-  portfolio: CustodyAssetBalance[];
-  currency: FiatCurrency;
-  isLoading: boolean;
-}
-
-export const Portfolio = ({ portfolio, currency, isLoading }: PortfolioProps) => {
-  const { translate } = useSettingsContext();
-
-  return isLoading ? (
-    <div className="w-full flex flex-col items-center justify-center gap-2 p-4">
-      <StyledLoadingSpinner size={SpinnerSize.LG} />
-    </div>
-  ) : portfolio?.length ? (
-    <StyledVerticalStack full gap={2}>
-      <StyledDataTable alignContent={AlignContent.BETWEEN}>
-        {portfolio.map((custodyAsset: CustodyAssetBalance) => (
-          <StyledDataTableRow key={custodyAsset.asset.name}>
-            <div className="w-full flex flex-row justify-between items-center gap-2 text-dfxBlue-800 p-2">
-              <div className="w-full flex flex-row items-center gap-3">
-                <DfxAssetIcon asset={custodyAsset.asset.name as AssetIconVariant} size={AssetIconSize.LG} />
-                <div className="text-base flex flex-col font-semibold text-left leading-none gap-1 pb-1">
-                  {custodyAsset.asset.name}
-                  <div className="text-sm text-dfxGray-700">{custodyAsset.asset.description}</div>
-                </div>
-              </div>
-              <div className="text-base text-right w-full flex flex-col font-semibold leading-none gap-1 pb-1 pr-1">
-                {formatCurrency(custodyAsset.balance, 0, 5)}
-                <div className="text-sm text-dfxGray-700">{`${formatCurrency(
-                  custodyAsset.value,
-                  2,
-                  2,
-                )} ${currency}`}</div>
-              </div>
-            </div>
-          </StyledDataTableRow>
-        ))}
-      </StyledDataTable>
-    </StyledVerticalStack>
-  ) : (
-    <div className="w-full flex flex-col items-center justify-center gap-2 p-4">
-      <p className="text-dfxBlue-300 text-left">{translate('screens/safe', 'No assets found')}</p>
-    </div>
-  );
-};
