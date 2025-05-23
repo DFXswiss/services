44f0b94276921b9cc780884ada8a402b10c7c654
Patrick <42653152+lapatric@users.noreply.github.com>
2025-05-23 14:10:06 +0200

    [NO-TASK] various improvements (#594)
    * [NO-TASK] fix 401 unauthorized on logout
    
    * [NO-TASK] refactor qr-code
    
    * [NO-TASK] mail login refactor
    
    * [NO-TASK] mail login refactor 2

--- src/components/payment/qr-code.tsx
+++ src/components/payment/qr-code.tsx
@@ -9,7 +9,7 @@ interface QrCopyProps {
 
 export function QrCopy({ data }: QrCopyProps): JSX.Element {
   return (
-    <div className="relative w-full max-w-[20rem]">
+    <div className="relative flex justify-center w-full max-w-[20rem]">
       <div
         className="absolute bg-white border p-1 rounded-sm top-1/2 left-1/2"
         style={{ transform: 'translate(-50%, -50%)' }}
@@ -21,14 +21,31 @@ export function QrCopy({ data }: QrCopyProps): JSX.Element {
   );
 }
 
+function stringIsSVG(value: string): boolean {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(value, 'image/svg+xml');
+  const svgElement = doc.getElementsByTagName('svg')[0];
+  return svgElement !== undefined && svgElement.nodeName === 'svg';
+}
+
 export function QrBasic({ data, isLoading }: QrCopyProps): JSX.Element {
+  const isSVG = stringIsSVG(data);
+
   return (
-    <div className={`p-2 border border-dfxGray-500 rounded-md ${isLoading ? 'animate-pulse border-dfxGray-300' : ''}`}>
-      <QRCode
-        className="mx-auto h-auto w-full max-w-[15rem] rounded-sm"
-        value={data}
-        fgColor={isLoading ? '#0000000A' : '#072440'}
-      />
+    <div
+      className={`p-2 h-auto w-full max-w-[15rem] border border-dfxGray-500 rounded-md flex justify-center items-center ${
+        isLoading ? 'animate-pulse border-dfxGray-300' : ''
+      }`}
+    >
+      {isSVG ? (
+        <img
+          className="h-full w-full rounded-sm"
+          src={`data:image/svg+xml;utf8,${encodeURIComponent(data)}`}
+          alt="Swiss QR Bill"
+        />
+      ) : (
+        <QRCode className="h-full w-full rounded-sm" value={data} fgColor={isLoading ? '#0000000A' : '#072440'} />
+      )}
     </div>
   );
 }
