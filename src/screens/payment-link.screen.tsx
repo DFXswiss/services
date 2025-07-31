5d47a1a362a61bfd537e6677e91fbb6e2e4f4f59
lapatric <42653152+lapatric@users.noreply.github.com>
2025-07-31 16:22:02 +0200

    [NO-TASK] add SPAR locations map

--- src/screens/payment-link.screen.tsx
+++ src/screens/payment-link.screen.tsx
@@ -692,14 +692,14 @@ export default function PaymentLinkScreen(): JSX.Element {
             </>
           )}
 
-          <div className="flex flex-col gap-2 mt-6 w-full">
-            {/* <div className="w-full h-96 rounded-md overflow-clip">
+          <div className="flex flex-col gap-4 mt-6 w-full">
+            <div className="w-full h-96 rounded-md overflow-clip">
               <iframe
-                src="https://www.google.com/maps/d/embed?mid=1DzX6z5tnUqn1zlzFnL6G58xREItorRM&ehbc=2E312F"
+                src="https://www.google.com/maps/d/embed?mid=1DzX6z5tnUqn1zlzFnL6G58xREItorRM&ehbc=2E312F&noprof=1"
                 width="100%"
                 height="100%"
               ></iframe>
-            </div> */}
+            </div>
             <div className="w-full leading-none">
               <StyledButton
                 label={translate('screens/payment', 'Learn more about OpenCryptoPay')}
