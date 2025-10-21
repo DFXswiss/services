df0e603db1943d1b32635fc1ff47012153863bd8
David May <david.leo.may@gmail.com>
2025-10-21 10:26:44 +0200

    [DEV-4446] Compliance search type

--- src/hooks/compliance.hook.ts
+++ src/hooks/compliance.hook.ts
@@ -2,6 +2,26 @@ import { AccountType, KycStatus, ResponseType, useApi } from '@dfx.swiss/react';
 import { useMemo } from 'react';
 import { downloadFile, filenameDateFormat } from 'src/util/utils';
 
+export enum ComplianceSearchType {
+  REF = 'Ref',
+  KYC_HASH = 'KycHash',
+  BANK_USAGE = 'BankUsage',
+  MAIL = 'Mail',
+  USER_ADDRESS = 'UserAddress',
+  DEPOSIT_ADDRESS = 'DepositAddress',
+  TXID = 'TxId',
+  ACCOUNT_SERVICE_REF = 'AccountServiceRef',
+  USER_DATA_ID = 'UserDataId',
+  IP = 'IP',
+  PHONE = 'Phone',
+  NAME = 'Name',
+}
+
+export interface ComplianceSearchResult {
+  type: ComplianceSearchType;
+  userDatas: UserSearchResult[];
+}
+
 export interface UserSearchResult {
   id: number;
   kycStatus: KycStatus;
@@ -12,6 +32,13 @@ export interface UserSearchResult {
 
 export function useCompliance() {
   const { call } = useApi();
+
+  async function search(key: string): Promise<ComplianceSearchResult> {
+    return call<ComplianceSearchResult>({
+      url: `support?key=${encodeURIComponent(key)}`,
+      method: 'GET',
+    });
+  }
 
   async function downloadUserData(userDataIds: number[]): Promise<void> {
     const { data, headers } = await call<{ data: Blob; headers: Record<string, string> }>({
@@ -24,12 +51,5 @@ export function useCompliance() {
     downloadFile(data, headers, `DFX_export_${filenameDateFormat()}.zip`);
   }
 
-  async function searchUsers(key: string): Promise<UserSearchResult[]> {
-    return call<UserSearchResult[]>({
-      url: `support?key=${encodeURIComponent(key)}`,
-      method: 'GET',
-    });
-  }
-
-  return useMemo(() => ({ downloadUserData, searchUsers }), [call]);
+  return useMemo(() => ({ search, downloadUserData }), [call]);
 }
