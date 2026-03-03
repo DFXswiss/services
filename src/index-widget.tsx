3b9f160e44965dbb5652bfeadfb46b1ebb16bddd
Bernd <bernd@dfx.swiss>
2026-03-03 07:16:13 +0100

    add key parameter

--- src/index-widget.tsx
+++ src/index-widget.tsx
@@ -11,6 +11,7 @@ const props: { [k in keyof WidgetParams]: 'string' | 'number' | 'boolean' | 'fun
   lang: 'string',
   address: 'string',
   signature: 'string',
+  key: 'string',
   mail: 'string',
   accountType: 'string',
   firstName: 'string',
